8b7fa6a4d87ec4e0e2e529b80644a74be085516c
matthew <matthewstafford29@gmail.com>
2025-02-07 15:01:57 -0500

    binary search

--- src/binary_search/binary_search.py
+++ src/binary_search/binary_search.py
@@ -7,14 +7,68 @@ class BinarySearch:
     def binary_search(self, input: List[int], search: int):
         if len(input) == 0:
             return -1
-        left_pntr = 0
-        right_pntr = len(input)-1
-        while left_pntr <= right_pntr:
-            middle_index = (left_pntr + right_pntr) // 2
-            if search == input[middle_index]:
+        pntr_left = 0
+        pntr_right = len(input) - 1
+        while pntr_left <= pntr_right:
+            middle_index = pntr_left + ((pntr_right - pntr_left) // 2)
+
+            if input[middle_index] == search:
                 return middle_index
-            elif search > input[middle_index]:
-                left_pntr = middle_index + 1
+            elif input[middle_index] < search:
+                pntr_left = middle_index + 1
             else:
-                right_pntr = middle_index - 1
+                pntr_right = middle_index - 1
         return -1
+
+    def binary_search_recur(self, input: List[int], search: int):
+        return self.__binary_search_recur_helper__(input, search, 0, len(input) - 1)
+
+    def __binary_search_recur_helper__(self, input: List[int], search: int, left: int, right: int):
+        if left > right:
+            return -1
+        if input[self.__middle_index__(left, right)] == search:
+            return self.__middle_index__(left, right)
+        elif input[self.__middle_index__(left, right)] < search:
+            return self.__binary_search_recur_helper__(input, search, self.__middle_index__(left, right) + 1, right)
+        else:
+            return self.__binary_search_recur_helper__(input, search, left, self.__middle_index__(left, right) - 1)
+
+    def __middle_index__(self, left: int, right: int):
+        return left + ((right - left) // 2)
+
+    def find_insertion_index(self, input_arr: List[int], target: int):
+        """
+        You are given a sorted array that contains unique values, along with a n integer target.
+        * if the array contains the target value return the index.
+        * otherwise return the insertion index. This is the index where the target would be if it were interacted in order,
+         maintaining the sorted sequence of the array.
+        """
+        return self.__find_insertion_index__(input_arr, target, 0, len(input_arr) - 1)
+
+    def __find_insertion_index__(self, input: List[int], search: int, left: int, right: int):
+        if left > right:
+            return left
+        if input[self.__middle_index__(left, right)] == search:
+            return self.__middle_index__(left, right)
+        elif input[self.__middle_index__(left, right)] < search:
+            return self.__find_insertion_index__(input, search, self.__middle_index__(left, right) + 1, right)
+        else:
+            return self.__find_insertion_index__(input, search, left, self.__middle_index__(left, right) - 1)
+
+    def cutting_wood(self, input_wood: List[int], at_least: int) -> int:
+        return self.__cutting_wood_recur_helper__(input_wood, at_least, 0, max(input_wood))
+
+    def determine_wood_at_height(self, input_wood: List[int], height: int):
+        return sum((wood - height) if wood > height else 0 for wood in input_wood)
+
+    def __cutting_wood_recur_helper__(self, input: List[int], search: int, left: int, right: int):
+        if left > right:
+            return left
+        found = self.determine_wood_at_height(input, self.__middle_index__(left, right) + 1)
+        print(f"\n{left} {right} {found} looking for {search}")
+        if found == search:
+            return self.__middle_index__(left, right)
+        elif found < search:
+            return self.__cutting_wood_recur_helper__(input, search, left, self.__middle_index__(left, right) - 1)
+        else:
+            return self.__cutting_wood_recur_helper__(input, search, self.__middle_index__(left, right) + 1, right)
--- tests/binary_search_test/test_binary_search.py
+++ tests/binary_search_test/test_binary_search.py
@@ -1,16 +1,39 @@
 import unittest
 
+from parameterized import parameterized
+import pytest
+
 from binary_search.binary_search import BinarySearch
 
 
 class BinarySearchTest(unittest.TestCase):
 
-    def test_binary_search(self):
+    @parameterized.expand([
+        ([1, 2, 3, 4], 1, 0),
+        ([1, 2, 3, 4], 2, 1),
+        ([1, 2, 3, 4], 3, 2),
+        ([1, 2, 3, 4], 4, 3),
+        ([1], 1, 0),
+        ([], 1, -1),
+        ([1, 2, 3, 4], 45, -1)])
+    def test_binary_search(self, input_list, target, expected):
         bs = BinarySearch()
-        assert bs.binary_search([1, 2, 3, 4], 1) == 0
-        assert bs.binary_search([1, 2, 3, 4], 2) == 1
-        assert bs.binary_search([1, 2, 3, 4], 3) == 2
-        assert bs.binary_search([1, 2, 3, 4], 4) == 3
-        assert bs.binary_search([1], 1) == 0
-        assert bs.binary_search([], 1) == -1
-        assert bs.binary_search([1, 2, 3, 4], 45) == -1
+        self.assertEqual(bs.binary_search(input_list, target), expected)
+        self.assertEqual(bs.binary_search_recur(input_list, target), expected)
+
+
+    @parameterized.expand([
+        ([1, 2, 4, 5, 7, 8, 9], 4, 2),
+        ([1, 2, 4, 5 ,7, 8, 9], 6, 4)
+    ])
+    def test_binary_search(self, input_list, target, expected):
+        bs = BinarySearch()
+        self.assertEqual(bs.find_insertion_index(input_list, target), expected)
+        self.assertEqual(bs.find_insertion_index(input_list, target), expected)
+
+    @parameterized.expand([
+        ([2,6,3,8], 7, 3),
+    ])
+    def test_cutting_wood(self, input_list, target, expected):
+        bs = BinarySearch()
+        self.assertEqual(bs.cutting_wood(input_list, target), expected)
