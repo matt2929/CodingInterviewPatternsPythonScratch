e1819bd615839488dc3f5ace3ac839ee20bfe873
matthew <matthewstafford29@gmail.com>
2025-02-08 16:41:09 -0500

    heap to find middle

--- src/heaps/heaps.py
+++ src/heaps/heaps.py
@@ -3,12 +3,14 @@ import heapq
 from dataclasses import dataclass
 from typing import Any, List
 
+from linked_lists.model import SingleLinkedListNode
+
 
 class Heap:
 
     def k_most_frequent_str(self, k: int, input_strs: List[str]):
         count_times_present = collections.Counter(input_strs)
-        priority_nodes = [PriorityNode(priority=v, payload=k) for k, v in count_times_present.items()]
+        priority_nodes = [MinPriorityNode(priority=v, payload=k) for k, v in count_times_present.items()]
         heap = []
         heapq.heapify(heap)
         for i in priority_nodes:
@@ -22,13 +24,56 @@ class Heap:
         output.reverse()
         return output
 
+    def combine_linked_lists(self, input_nodes: List[SingleLinkedListNode]):
+        heap = []
+        heapq.heapify(heap)
+        output_head = SingleLinkedListNode(-1, None)
+        running_node = output_head
+        for i in input_nodes:
+            if i:
+                heapq.heappush(heap, MinPriorityNode(priority=i.val, payload=i))
+        while heap:
+            top_node = heapq.heappop(heap)
+            print(f"\nh: {heap}\n\tt: {top_node}")
+            running_node.next = SingleLinkedListNode(top_node.priority, None)
+            running_node = running_node.next
+            if top_node.payload.next:
+                heapq.heappush(heap, MinPriorityNode(priority=top_node.payload.next.val, payload=top_node.payload.next))
+        return output_head.next
+
+    def find_median(self, input_list: List[int]) -> int:
+        left_heap = []
+        right_heap = []
+        for i in input_list:
+            print(
+                f"\n\t1:{left_heap} {left_heap[0] if left_heap else None}\n\t2:{right_heap} {right_heap[0] if right_heap else None}")
+            if not left_heap or i <= left_heap[0]:
+                heapq.heappush(left_heap, -1 * i)
+            else:
+                heapq.heappush(right_heap, i)
+            if len(left_heap) > len(right_heap) + 1:
+                heapq.heappush(right_heap, -1 * heapq.heappop(left_heap))
+            if len(right_heap) > len(left_heap):
+                heapq.heappush(left_heap, -1 * heapq.heappop(right_heap))
+        if len(left_heap) > len(right_heap):
+            return -1 * heapq.heappop(left_heap)
+        else:
+            return (-1 * heapq.heappop(left_heap) + (heapq.heappop(right_heap))) / 2
+
 
 @dataclass
-class PriorityNode:
+class MinPriorityNode:
     priority: int
-    payload: str
+    payload: any
 
     def __lt__(self, other):
-        if self.priority == other.priority:
-            return self.payload < other.payload
         return self.priority < other.priority
+
+
+@dataclass
+class MaxPriorityNode:
+    priority: int
+    payload: any
+
+    def __lt__(self, other):
+        return self.priority > other.priority
--- tests/heaps_tests/heap_tests.py
+++ tests/heaps_tests/heap_tests.py
@@ -3,6 +3,7 @@ import unittest
 from parameterized import parameterized
 
 from heaps.heaps import Heap
+from linked_lists.model import SingleLinkedListNode
 from stacks.stack_stuff import StackStuff
 
 
@@ -16,3 +17,22 @@ class HeapStuffTests(unittest.TestCase):
     def test_reverse_list(self, input, k, expected):
         ss = Heap()
         assert ss.k_most_frequent_str(k, input) == expected
+
+    def test_combined_linked_list(self):
+        input_test = [
+            SingleLinkedListNode(1, SingleLinkedListNode(6, None)),
+            SingleLinkedListNode(1, SingleLinkedListNode(4, SingleLinkedListNode(6, None))),
+            SingleLinkedListNode(3, SingleLinkedListNode(7, None)),
+        ]
+        ss = Heap()
+        output_head = ss.combine_linked_lists(input_test)
+        assert output_head.linked_list_as_array_str() == "[1, 1, 3, 4, 6, 6, 7]"
+
+    @parameterized.expand([
+        ([0, 1, 2, 2, 3, 4, 5, 7, 8], 3),
+        ([0, 1, 2], 1),
+        ([4, 3, 1, 10], 3.5),
+    ])
+    def test_reverse_list(self, input_list, expected):
+        ss = Heap()
+        assert ss.find_median(input_list) == expected
