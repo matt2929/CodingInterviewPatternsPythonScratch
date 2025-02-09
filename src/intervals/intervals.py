739a5ea40bcff7b4ffecd975bee42ffbfd325119
matthew <matthewstafford29@gmail.com>
2025-02-09 14:48:00 -0500

    overlapping sets

--- src/intervals/intervals.py
+++ src/intervals/intervals.py
@@ -23,3 +23,25 @@ class INTERVALS:
         if temp_interval:
             output.append(temp_interval)
         return output
+
+    def identify_all_interval_overlaps(
+            self,
+            input_intervals_a: List[Tuple[int, int]],
+            input_intervals_b: List[Tuple[int, int]]
+    ) -> List[Tuple[int, int]]:
+        left_pointer = 0
+        right_pointer = 0
+        running_overlaps = []
+        while (left_pointer < len(input_intervals_a) and right_pointer < len(input_intervals_b)):
+            interval_a = input_intervals_a[left_pointer]
+            interval_b = input_intervals_b[right_pointer]
+
+            if self.do_intervals_overlap(interval_a, interval_b):
+                temp_interval = (max(interval_a[0], interval_b[0]), min(interval_a[1], interval_b[1]))
+
+                running_overlaps.append(temp_interval)
+            if interval_a[1] < interval_b[1]:
+                left_pointer += 1
+            else:
+                right_pointer += 1
+        return running_overlaps
