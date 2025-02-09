5260214e7beeafb8b8e832ce952435c1a801b33f
matthew <matthewstafford29@gmail.com>
2025-02-09 14:14:54 -0500

    merge overlapping intervals

--- src/new_chapter.sh
+++ src/new_chapter.sh
@@ -10,7 +10,7 @@ cd "${lower_case_name}"
 touch __init__.py
 touch "${lower_case_name}.py"
 echo "class ${upper_case_name}:" >> "${lower_case_name}.py"
-echo "  ..." >> "${lower_case_name}.py"
+echo "    ..." >> "${lower_case_name}.py"
 cd ../..
 cd tests
 mkdir "${lower_case_name}_tests"
@@ -18,7 +18,16 @@ cd "${lower_case_name}_tests"
 touch __init__.py
 touch "${lower_case_name}_tests.py"
 echo "import unittest" >> "${lower_case_name}_tests.py"
+echo "from parameterized import parameterized" >> "${lower_case_name}_tests.py"
 echo "from ${lower_case_name}.${lower_case_name} import ${upper_case_name}" >> "${lower_case_name}_tests.py"
 echo "" >> "${lower_case_name}_tests.py"
-echo "class ${upper_case_name}Tests(unittest.unittests):" >> "${lower_case_name}_tests.py"
-echo "  ${lower_case_name} = ${upper_case_name}()" >> "${lower_case_name}_tests.py"
+echo "class ${upper_case_name}Tests(unittest.TestCase):" >> "${lower_case_name}_tests.py"
+
+echo "" >> "${lower_case_name}_tests.py"
+echo "
+  @parameterized.expand([
+        ('a', False),
+        ('b', False),
+    ])
+  def test_${lower_case_name}(self, input, expected):
+      ${lower_case_name} = ${upper_case_name}()" >> "${lower_case_name}_tests.py"
